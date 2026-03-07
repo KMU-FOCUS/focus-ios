7e88980c604c8d268b02f8a2f26c56a71188a1d8
47813 <vanguard0956@gmail.com>
2026-03-07 18:19:22 +0900

    Initial Commit

---
+++ focusTests/focusTests.swift
@@ -0,0 +1,17 @@
+//
+//  focusTests.swift
+//  focusTests
+//
+//  Created by 이동언 on 3/7/26.
+//
+
+import Testing
+@testable import focus
+
+struct focusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
